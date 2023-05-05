daf2c45f9f36cf2fd4c1a6b1e4e65b7185c1f7af
jaiswalk008 <jaiskaran008@gmail.com>
2023-05-05 17:33:59 +0530

    adding response

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const http = require('http');
 const server  = http.createServer((req,res)=>{
-   // console.log(req);
-    console.log('karan jaiswal')
-    
+   const url = req.url;
+   if(url==='/home'){
+    res.write('<html>');
+    res.write('<head><title>Nodejs Tutorial</title></head>');
+    res.write('<body><h1>Welcome Home</h1></body>');
+    res.write('</html>');
+    return res.end();
+   }
+   else if(url==='/about'){
+    res.write('<html>');
+    res.write('<head><title>Nodejs Tutorial</title></head>');
+    res.write('<body><h1>Welcome to About Us page</h1></body>');
+    res.write('</html>');
+    return res.end();
+   }
+   else if(url==='/node'){
+    res.write('<html>');
+    res.write('<head><title>Nodejs Tutorial</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    return res.end();
+   }
+
+   res.setHeader('Content-Type','text/html');
+   res.write('<html>');
+    res.write('<head><title>Nodejs Tutorial</title></head>');
+    res.write('<body><h1>Welcome</h1></body>');
+    res.write('</html>');
+    res.end(); 
 })
 server.listen(4000);
--- first-app.js
+++ first-app.js
@@ -1,2 +1,29 @@
-console.log('hello world');
-//node first-app.js
+console.log('a');
+console.log('b');
+
+async function  func(){
+    const pr1 = await new Promise((resolve)=>{
+        setTimeout(()=>{
+
+            console.log('c');
+      
+            resolve();
+      
+          },3000);
+    })
+    const pr2 = await new Promise((resolve)=>{
+        setTimeout(()=>{
+
+            console.log('d');
+      
+            resolve();
+      
+          },0);
+    })
+    console.log('e')
+
+}
+
+func();
+
+
